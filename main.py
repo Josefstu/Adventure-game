fb233670c48da334a9538060e00b495e7d7308fc
Josefstu <59778458+Josefstu@users.noreply.github.com>
2022-01-25 18:37:54 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Player Stats
+
+health=100
+eneHealth = 10
+eneDam = 20
+
+
+
+
+print("You awake on a hard stone floor. In a room illuminated by torches mounted to the wall. The damp and cold leads you to believe you may be underground \n"
+      "As you begin to stand you see you can follow the long hallway ahead. Will you embark on this adventure? y/n")
+
+StartGame = input()
+if StartGame == 'y':
+    pass
+if StartGame == 'n':
+    print("This was not optional, try again")
+
+print("As you come to your feet you see something illuminating at the end of the hall, you begin walking towards it")
+
+
+
+
+
+
+
+
+
+

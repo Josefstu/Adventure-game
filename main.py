0f50d01caf0529870974552fa767dbadfbf59d1f
Josefstu <59778458+Josefstu@users.noreply.github.com>
2022-01-26 22:00:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-
+import time
 
 
 print('''
@@ -17,7 +17,7 @@ print('''
 
 
 print("You awake on a hard stone floor. In a room illuminated by torches mounted to the wall. The damp and cold leads you to believe you may be underground \n"
-      "As you begin to stand you see you can follow the long hallway ahead. Will you embark on this adventure? y/n")
+      "As you begin to stand you see you can follow the long hallway ahead. Will you embark on this adventure? y/n \n")
 
 StartGame = input()
 if StartGame == 'y':
@@ -25,7 +25,39 @@ if StartGame == 'y':
 
 
 print("As you come to your feet you see something illuminating at the end of the hall, you begin walking towards it")
+time.sleep(1)
+print('''As you approach closer to the light you notice a humanoid figure, as it turns around you see this is no human
+The being lunges towards you, you grab the sword from its sheath and prepare to fight... \n''')
+time.sleep(5)
 
+print('''      
+                               ,--,  ,.-.
+               ,                   \,       '-,-`,'-.' | ._
+              /|           \    ,   |\         }  )/  / `-,',
+              [ ,          |\  /|   | |        /  \|  |/`  ,`
+              | |       ,.`  `,` `, | |  _,...(   (      .',
+              \  \  __ ,-` `  ,  , `/ |,'      Y     (   /_L
+               \  \_\,``,   ` , ,  /  |         )         _,/
+                \  '  `  ,_ _`_,-,<._.<        /         /
+                 ', `>.,`  `  `   ,., |_      |         /
+                   \/`  `,   `   ,`  | /__,.-`    _,   `
+               -,-..\  _  \  `  /  ,  / `._) _,-\`       
+                \_,,.) /\    ` /  / ) (-,, ``    ,        |
+               ,` )  | \_\       '-`  |  `(                
+              /  /```(   , --, ,' \   |`<`    ,            |
+             /  /_,--`\   <\  V /> ,` )<_/)  | \      _____)
+       ,-, ,`   `   (_,\ \    |   /) / __/  /   `----`
+      (-, \           ) \ ('_.-._)/ /,`    /
+      | /  `          `/ \  V   V, /`     /
+   ,--\(        ,     <_/`\      ||      /
+  (   ,``-     \/|         \-A.A-`|     /
+ ,>,_ )_,..(    )\          -,,_-`  _--`
+(_ \|`   _,/_  /  \_            ,--`
+ \( `   <.,../`     `-.._   _,-`     
+''')
+
+print("It's time to fight, choose your next move!")
+print("1. Attack 2. Heal")
 
 
 
